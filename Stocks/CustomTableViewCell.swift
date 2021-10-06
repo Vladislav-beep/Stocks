a672d21efee18b82e42d62d3332e4a2a0c526a10
VLADISLAV <SIZONOV.95@MAIL.RU>
2021-10-06 23:25:43 +0300

    commit

---
+++ Stocks/CustomTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CustomTableViewCell.swift
+//  Stocks
+//
+//  Created by Владислав Сизонов on 06.10.2021.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
